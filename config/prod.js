f0d76b0127ad6c9581657f097f479540e89c0b77
Noble Ibe <noble.ibe@rutgers.edu>
2020-03-20 18:02:41 -0400

    auth flow finished

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports ={
+    googleclientID: process.env.GOOGLE_CLIENT_ID
+    ,
+    googleclientSecret:process.env.GOOGLE_CLIENT_SECRET ,
+    mongoURI:process.env.MONGO_URI
+    ,
+    cookieKey: process.env.COOKIE_KEY
+
+
+};
