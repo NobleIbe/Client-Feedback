8f7e0a0c289584ea82b97f8ab9f6485b9025d7cf
Noble Ibe <noble.ibe@rutgers.edu>
2020-03-21 10:55:45 -0400

    fixes for heroku

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,7 @@
-module.exports ={
-    googleclientID: process.env.GOOGLE_CLIENT_ID
-    ,
-    googleclientSecret:process.env.GOOGLE_CLIENT_SECRET ,
-    mongoURI:process.env.MONGO_URI
-    ,
+module.exports = {
+    googleclientID: process.env.GOOGLE_CLIENT_ID,
+    googleclientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI:process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 
 
