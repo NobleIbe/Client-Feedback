483a8804b70efd6295c25bad6daeae3f986ac69d
Noble Ibe <noble.ibe@rutgers.edu>
2020-03-25 09:55:17 -0400

    hopefully this works

---
+++ config/dev.js
@@ -0,0 +1,14 @@
+module.exports ={
+    googleclientID: '322302108468-qohmrr65udsc0irt7k0gk1ctdseq5krj.apps.googleusercontent.com'
+    ,
+    googleclientSecret: '7CKx-_zSw8aY5x8iLZHIvNOR',
+    mongoURI:'mongodb+srv://NobleI:GGjWjFRS6ejnpZ0a@emaily-wk8rl.mongodb.net/test?retryWrites=true&w=majority'
+    ,
+    cookieKey: 'ashasusniqnneqnqeimkwklemm',
+    stripePublishableKey:'pk_test_yg9jzudjZfDxgxoReFfglXp500o1rWAo0x',
+    stripeSecretKey:'sk_test_nhbwwYcZP4aEJZjCYPI1WsmA00rkj7bmnR'
+
+
+
+
+};
